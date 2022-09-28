81479ab583b4291aace9e9641e03e9d829f923a6
Loredana <70909576+loredana-stirbu@users.noreply.github.com>
2022-09-28 08:02:01 +0300

    Update probleme.py

--- probleme.py
+++ probleme.py
@@ -18,6 +18,13 @@ def triunghiuri(a,b,c,d):
         perimetru=sp*2
         return print("Laturile",c,',',b,',',d, "pot forma un triunghi, astfel aria acestuia va fi: ",aria,", iar perimetrul: ",perimetru)
 
+    elif (c+a>d) and (c+d>a) and (d+a>c):
+        sp=(c+a+d)/2
+        aria=round(math.sqrt(sp*(sp-c)*(sp-a)*(sp-d)),3)
+        perimetru=sp*2
+        return print("Laturile",c,',',a,',',d, "pot forma un triunghi, astfel aria acestuia va fi: ",aria,", iar perimetrul: ",perimetru)
+
+
 x=int(input("Dati I-ul nr: "))
 y=int(input("Dati II-a nr: "))
 z=int(input("Dati III-a nr: "))
