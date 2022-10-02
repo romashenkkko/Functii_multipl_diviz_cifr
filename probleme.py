16704c288dc468bdc7ad41166228ddf1055c78f4
Loredana <70909576+loredana-stirbu@users.noreply.github.com>
2022-10-02 14:40:01 +0300

    Update probleme.py

--- probleme.py
+++ probleme.py
@@ -73,9 +73,9 @@ n=int(input("Dati X-a nr: "))
 #3
 def mediane(a,b,c):
     if (a+b>c) and (a+c>b) and (b+c>a):
-        ma=round(0,5*(math.sqrt((2*(b**2))+(2*(c**2))-(a**2))),3)
-        mb=round(0,5*(math.sqrt((2*(a**2))+(2*(c**2))-(b**2))),3)
-        mc=round(0,5*(math.sqrt((2*(b**2))+(2*(a**2))-(c**2))),3)
+        ma=round(0.5*(math.sqrt((2*(b**2))+(2*(c**2))-(a**2))),3)
+        mb=round(0.5*(math.sqrt((2*(a**2))+(2*(c**2))-(b**2))),3)
+        mc=round(0.5*(math.sqrt((2*(b**2))+(2*(a**2))-(c**2))),3)
         return print("Mediana laturei a:",a, "este:",ma,"; mediana laturei b:",b, "este:",mb,"mediana laturei c:",c, "este:",mc)
     else:
         return print("Laturile date nu pot forma un triunghi!")
